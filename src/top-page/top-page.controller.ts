a4032150a29b6298ab9d226c4ad7e32536315afb
Peter Tolkachev <ptolkachev@hse.ru>
2021-05-30 22:59:10 +0300

    fix db config

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,18 +1,23 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FindTopPageDto } from './dto/find-top-page.dto';
+import { TopPageModel } from './top-page.model';
 import { TopPageModule } from './top-page.module';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService: ConfigService) {
+
+  }
 
   @Post('create')
   async create(@Body() dto: Omit<TopPageModule, '_id'>){
 
   }
 
-  @Get(':id')
-  async get(@Param('id') id: string) {
-
+  @Get('get/:alias')
+  async get(@Param('alias') alias: string): Promise<TopPageModel> {
+	this.configService.get('TEST');
   }
 
   @Delete(':id')
